79e0d8bb691edbf54a885b547c4962040a933b8b
WillianFarias <williansousafarias@gmail.com>
2021-02-27 15:13:35 -0300

    criando primeira migration

---
+++ src/database/migrations/1614449593016-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1614449593016 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
