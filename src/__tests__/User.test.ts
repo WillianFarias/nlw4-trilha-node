98cb1c5f5c87ae748a5cfae7862c14b53b78287b
WillianFarias <williansousafarias@gmail.com>
2021-02-28 13:18:26 -0300

    Finalizacao dos teste

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,11 +1,32 @@
 import request from 'supertest';
 import { app } from '../app';
 
-describe("Users", () => {
+import createConnection from '../database';
 
-  request(app).post('/users').
+describe("Users", async () => {
+  //antes de tudo as migrations devem ser rodadas
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it("Shout be able to create a new user", async() => {
+    const response = await request(app).post('/users').
     send({
       email: "user@example.com",
       name: "User Example"
     });
+
+    expect(response.status).toBe(201);
+  })
+
+  it("Should not be able to create a user with exists email", async () => {
+    const response = await request(app).post('/users').
+    send({
+      email: "user@example.com",
+      name: "User Example"
+    });
+
+    expect(response.status).toBe(400);
+  })
 });
